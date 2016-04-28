2fd940021f1e5fa7db841ed82ff94f1e1df8d06e
Antonio Willy Malara <antonio.malara@gmail.com>
2016-04-28 02:34:26 +0200

    Initial Commit

---
+++ Sources/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  EasyCart
+//
+//  Created by Antonio Malara on 28/04/16.
+//  Copyright © 2016 Antonio Malara. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
